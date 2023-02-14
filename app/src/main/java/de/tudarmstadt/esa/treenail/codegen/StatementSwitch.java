b3535cee36c435d3ad3050030987090c5769c170
Julian Oppermann <oppermann@esa.tu-darmstadt.de>
2023-02-15 10:47:59 +1300

    ForLoop -> scf.while.

--- app/src/main/java/de/tudarmstadt/esa/treenail/codegen/StatementSwitch.java
+++ app/src/main/java/de/tudarmstadt/esa/treenail/codegen/StatementSwitch.java
@@ -9,13 +9,16 @@ import com.google.common.collect.Streams;
 import com.minres.coredsl.analysis.AnalysisContext;
 import com.minres.coredsl.analysis.StorageClass;
 import com.minres.coredsl.coreDsl.CompoundStatement;
+import com.minres.coredsl.coreDsl.Declaration;
 import com.minres.coredsl.coreDsl.DeclarationStatement;
 import com.minres.coredsl.coreDsl.ExpressionInitializer;
 import com.minres.coredsl.coreDsl.ExpressionStatement;
+import com.minres.coredsl.coreDsl.ForLoop;
 import com.minres.coredsl.coreDsl.IfStatement;
 import com.minres.coredsl.coreDsl.NamedEntity;
 import com.minres.coredsl.coreDsl.util.CoreDslSwitch;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -41,8 +44,7 @@ class StatementSwitch extends CoreDslSwitch<Object> {
   }
 
   @Override
-  public Object caseDeclarationStatement(DeclarationStatement declStmt) {
-    var decl = declStmt.getDeclaration();
+  public Object caseDeclaration(Declaration decl) {
     assert decl.getQualifiers().isEmpty()
         : "NYI: Const/volatile for local variables";
 
@@ -66,6 +68,12 @@ class StatementSwitch extends CoreDslSwitch<Object> {
       cc.setValue(dtor, castValue);
     }
 
+    return this;
+  }
+
+  @Override
+  public Object caseDeclarationStatement(DeclarationStatement declStmt) {
+    doSwitch(declStmt.getDeclaration());
     return this;
   }
 
@@ -144,6 +152,108 @@ class StatementSwitch extends CoreDslSwitch<Object> {
         cast, resultTypes.stream().map(Object::toString).collect(joining(", ")),
         thenCC.getStringBuilder().toString().indent(N_SPACES),
         elseCC.getStringBuilder().toString().indent(N_SPACES));
+
+    return this;
+  }
+
+  @Override
+  public Object caseForLoop(ForLoop loop) {
+    var startDecl = loop.getStartDeclaration();
+    var startExpr = loop.getStartExpression();
+    var condExpr = loop.getCondition();
+    var loopExprs = loop.getLoopExpressions();
+    var bodyStmt = loop.getBody();
+
+    // Elaborate/evaluate the init-part of the header in the current context.
+    if (startDecl != null)
+      doSwitch(startDecl);
+    if (startExpr != null)
+      exprSwitch.doSwitch(startExpr);
+
+    // Simulate construction to find loop-carried values, in lieu of proper
+    // analysis.
+    var simCC = new ConstructionContext(
+        new LinkedHashMap<NamedEntity, MLIRValue>(cc.getValues()),
+        new AtomicInteger(cc.getCounter()), ac, new StringBuilder());
+    new ExpressionSwitch(simCC).doSwitch(condExpr);
+    new StatementSwitch(simCC).doSwitch(bodyStmt);
+    loopExprs.forEach(new ExpressionSwitch(simCC)::doSwitch);
+
+    var loopCarriedVars = simCC.getUpdatedEntities().stream().toList();
+    simCC = null; // Throw away the dummy construction.
+
+    // Real construction begins here. See
+    // https://mlir.llvm.org/docs/Dialects/SCFDialect/#scfwhile-mlirscfwhileop
+    // for the use of "before" and "after" terms.
+    var beforeCC = new ConstructionContext(
+        new LinkedHashMap<NamedEntity, MLIRValue>(cc.getValues()),
+        new AtomicInteger(cc.getCounter()), ac, new StringBuilder());
+
+    var afterCC = new ConstructionContext(
+        new LinkedHashMap<NamedEntity, MLIRValue>(cc.getValues()),
+        new AtomicInteger(cc.getCounter()), ac, new StringBuilder());
+
+    var argTypes = new ArrayList<MLIRType>();
+    var beforeArgs = new LinkedHashMap<NamedEntity, MLIRValue>();
+    var afterArgs = new LinkedHashMap<NamedEntity, MLIRValue>();
+    var results = new ArrayList<MLIRValue>();
+    for (var v : loopCarriedVars) {
+      var type = mapType(ac.getDeclaredType(v));
+      argTypes.add(type);
+
+      beforeCC.setValue(v, beforeCC.makeAnonymousValue(type));
+      beforeArgs.put(v, beforeCC.getValue(v));
+
+      afterCC.setValue(v, afterCC.makeAnonymousValue(type));
+      afterArgs.put(v, afterCC.getValue(v));
+
+      results.add(cc.makeAnonymousValue(type));
+    }
+
+    var argTypesStr =
+        argTypes.stream().map(Object::toString).collect(joining(", "));
+
+    var cond = new ExpressionSwitch(beforeCC).doSwitch(condExpr);
+    var condCast = beforeCC.makeSignlessCast(cond);
+    var beforeCondVals =
+        loopCarriedVars.stream().map(beforeCC::getValue).toList();
+    beforeCC.emitLn(
+        "scf.condition(%s) %s : %s", condCast,
+        beforeCondVals.stream().map(Object::toString).collect(joining(", ")),
+        argTypesStr);
+
+    var bodySwitch = new StatementSwitch(afterCC);
+    bodySwitch.doSwitch(bodyStmt);
+
+    var loopExprSwitch = new ExpressionSwitch(afterCC);
+    loopExprs.forEach(loopExprSwitch::doSwitch);
+
+    var afterYieldVals =
+        loopCarriedVars.stream().map(afterCC::getValue).toList();
+    afterCC.emitLn(
+        "scf.yield %s : %s",
+        afterYieldVals.stream().map(Object::toString).collect(joining(", ")),
+        argTypesStr);
+
+    var beforeArgsStr =
+        loopCarriedVars.stream()
+            .map(e -> format("%s = %s", beforeArgs.get(e), cc.getValue(e)))
+            .collect(joining(", "));
+    var afterArgsStr =
+        loopCarriedVars.stream()
+            .map(e -> format("%s: %s", afterArgs.get(e), afterArgs.get(e).type))
+            .collect(joining(", "));
+    var resultsStr =
+        results.stream().map(Object::toString).collect(joining(", "));
+
+    cc.emitLn("%s = scf.while (%s) : (%s) -> (%s) {\n%s} do {\n^bb0(%s):\n%s}",
+              resultsStr, beforeArgsStr, argTypesStr, argTypesStr,
+              beforeCC.getStringBuilder().toString().indent(N_SPACES),
+              afterArgsStr,
+              afterCC.getStringBuilder().toString().indent(N_SPACES));
+
+    Streams.forEachPair(loopCarriedVars.stream(), results.stream(),
+                        cc::setValue);
 
     return this;
   }
