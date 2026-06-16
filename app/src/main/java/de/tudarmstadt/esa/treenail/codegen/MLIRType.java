301bc918f73b1977e39c537f37354f958c548782
Benedikt <bfj.koehn@gmail.com>
2026-06-16 14:27:45 +0200

    Added general MLIRType.mapType that can be used when the type is irrelevant. (more specific versions should be preferred when we know which kind of type we have)

--- app/src/main/java/de/tudarmstadt/esa/treenail/codegen/MLIRType.java
+++ app/src/main/java/de/tudarmstadt/esa/treenail/codegen/MLIRType.java
@@ -1,6 +1,18 @@
 package de.tudarmstadt.esa.treenail.codegen;
 
+import com.minres.coredsl.type.CoreDslType;
+
 abstract class MLIRType {
   private static class VoidType extends MLIRType {}
   public static final MLIRType VOID = new VoidType();
+
+  static MLIRType mapType(CoreDslType type) {
+    if (type.isIntegerType()) {
+      return MLIRIntType.mapType(type);
+    } else if (type.isStructType()) {
+      return MLIRStructType.mapType(type);
+    }
+    assert false : "NYI: Union, array, enum types";
+    return null;
+  }
 }
