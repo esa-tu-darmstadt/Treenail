2b976d23ebd6b2c0191376080022da1d0d56b6fd
Benedikt <bfj.koehn@gmail.com>
2026-08-03 15:58:44 +0200

    Added exit when we hit a MemberAccessExpression, as it can't alias with integer members

--- app/src/main/java/de/tudarmstadt/esa/treenail/codegen/AliasAnalysis.java
+++ app/src/main/java/de/tudarmstadt/esa/treenail/codegen/AliasAnalysis.java
@@ -9,6 +9,7 @@ import com.minres.coredsl.coreDsl.Expression;
 import com.minres.coredsl.coreDsl.ExpressionInitializer;
 import com.minres.coredsl.coreDsl.ISA;
 import com.minres.coredsl.coreDsl.IndexAccessExpression;
+import com.minres.coredsl.coreDsl.MemberAccessExpression;
 import com.minres.coredsl.coreDsl.NamedEntity;
 import com.minres.coredsl.coreDsl.PostfixExpression;
 import com.minres.coredsl.coreDsl.PrefixExpression;
@@ -104,8 +105,15 @@ class AliasAnalysis {
 
   private static boolean containsOneOf(Expression expr,
                                        HashSet<NamedEntity> entities) {
+    // NOTE: There is no need to inspect the contents of the access, because
+    // if there is something like 'arr[i++]', it will be visited later
     while (expr instanceof IndexAccessExpression indexAccess) {
       expr = indexAccess.getTarget();
+    }
+    // Because our iterators are limited to integers, a member access
+    // expression can never reference the iterator value
+    if (expr instanceof MemberAccessExpression) {
+      return false;
     }
     assert expr instanceof EntityReference;
     EntityReference entityReference = (EntityReference)expr;
