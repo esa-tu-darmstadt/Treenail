2cb38ff0bce754e37267b3df67ad0cb997acd494
Benedikt <bfj.koehn@gmail.com>
2026-08-11 10:39:33 +0200

    Computed mlir type string once and made result of getMembers an unmodifyable map

--- app/src/main/java/de/tudarmstadt/esa/treenail/codegen/MLIRStructType.java
+++ app/src/main/java/de/tudarmstadt/esa/treenail/codegen/MLIRStructType.java
@@ -1,12 +1,37 @@
 package de.tudarmstadt.esa.treenail.codegen;
 
 import com.minres.coredsl.type.CoreDslType;
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 class MLIRStructType extends MLIRType {
   private final LinkedHashMap<String, MLIRType> members;
+  private final String mlirTypeString;
+
+  private static String
+  createStructTypeString(LinkedHashMap<String, MLIRType> members) {
+    StringBuilder sb = new StringBuilder("!hw.struct<");
+    final int lastIdx = members.size() - 1;
+    int currIdx = 0;
+    for (var item : members.entrySet()) {
+      String key = item.getKey();
+      MLIRType val = item.getValue();
+      sb.append(key);
+      sb.append(": ");
+      sb.append(val);
+      if (currIdx != lastIdx) {
+        sb.append(", ");
+      }
+      ++currIdx;
+    }
+    sb.append(">");
+    return sb.toString();
+  }
+
   private MLIRStructType(LinkedHashMap<String, MLIRType> members) {
     this.members = members;
+    this.mlirTypeString = createStructTypeString(members);
   }
 
   private static final LinkedHashMap<String, MLIRStructType> types =
@@ -37,25 +62,9 @@ class MLIRStructType extends MLIRType {
     return members.get(memberName);
   }
 
-  // TODO: this allows the user to modify members
-  public LinkedHashMap<String, MLIRType> getMembers() { return members; }
-  // TODO: it may make sense to compute this once and store it
-  public String toString() {
-    StringBuilder sb = new StringBuilder("!hw.struct<");
-    final int lastIdx = members.size() - 1;
-    int currIdx = 0;
-    for (var item : members.entrySet()) {
-      String key = item.getKey();
-      MLIRType val = item.getValue();
-      sb.append(key);
-      sb.append(": ");
-      sb.append(val);
-      if (currIdx != lastIdx) {
-        sb.append(", ");
-      }
-      ++currIdx;
-    }
-    sb.append(">");
-    return sb.toString();
+  public Map<String, MLIRType> getMembers() {
+    return Collections.unmodifiableMap(members);
   }
+
+  public String toString() { return mlirTypeString; }
 }
