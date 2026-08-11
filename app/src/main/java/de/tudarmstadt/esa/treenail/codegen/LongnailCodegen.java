4f2117cd6a48b08a36bef72e1f5bb38706a71188
Benedikt <bfj.koehn@gmail.com>
2026-08-11 10:55:56 +0200

    Added assert for volatile struct members. Const should be fine because const is already checked by the frontend

--- app/src/main/java/de/tudarmstadt/esa/treenail/codegen/LongnailCodegen.java
+++ app/src/main/java/de/tudarmstadt/esa/treenail/codegen/LongnailCodegen.java
@@ -157,8 +157,7 @@ public class LongnailCodegen implements ValidationMessageAcceptor {
       if (typeDecl instanceof StructTypeDeclaration structDecl) {
         var members = new LinkedHashMap<String, MLIRType>();
         for (var member : structDecl.getMembers()) {
-          // TODO: qualifiers
-          // - structs with volatile members exist
+          assert !member.getQualifiers().contains(TypeQualifier.VOLATILE) : "NYI: Volatile struct members";
           var memberType =
               MLIRType.mapType(ctx.getSpecifiedType(member.getType()));
           for (var dtor : member.getDeclarators()) {
